36c1ffebdaac5d37b2ae59edd7268824549fbc03
ushasri2645 <ugudikandula@everest.engineering>
2025-07-11 19:18:32 +0530

    Wrap the app properly in provider

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,16 +1,41 @@
-import { render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
-import { App } from './App';
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import { App } from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { CitiesProvider } from "../contexts/Cities/CitiesProvider";
 
-describe('App Component', () => {
-  it('renders Navbar component', () => {
-    render(<App />);
+describe("App Component", () => {
+  let queryClient: QueryClient;
+  beforeEach(() => {
+    queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+        },
+      },
+    });
+  });
+  it("renders Navbar component", () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <CitiesProvider>
+          <App />
+        </CitiesProvider>
+      </QueryClientProvider>
+    );
+
     const navTextInstances = screen.getAllByText(/CloudTrip/i);
     expect(navTextInstances.length).toBeGreaterThanOrEqual(1);
   });
 
-  it('renders Footer component', () => {
-    render(<App />);
+  it("renders Footer component", () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <CitiesProvider>
+          <App />
+        </CitiesProvider>
+      </QueryClientProvider>
+    );
     const footerText = screen.getByText(/All rights reserved/i);
     expect(footerText).toBeInTheDocument();
   });
