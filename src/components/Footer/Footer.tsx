16c784f635287f1d5d58f39c7a0955ca8c2cc210
jangamkeerthi26 <keerthi.jangam@everest.engineeing>
2025-07-11 12:57:12 +0530

    Add footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,23 @@
+import logo from "../../assets/images/logo_no_name-nobg.png";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.inner}>
+        <div className={styles.logoText}>
+          <img src={logo} alt="CloudTrip Logo" className={styles.navbarLogo} />
+          <span>© {currentYear} CloudTrip | All rights reserved.</span>
+        </div>
+        <div className={styles.links}>
+          <a href="#">About Us</a> | <a href="#">Contact</a> |{" "}
+          <a href="#">Privacy Policy</a> | <a href="#">Terms of Service</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
