cb0b9a7b3e0ff442cced43233f2ee4be0d9dd69c
ushasri2645 <ugudikandula@everest.engineering>
2025-07-11 19:18:52 +0530

    Add Search form in the App.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,12 @@
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
+import { FlightSearchForm } from "../components/SearchForm/SearchForm";
 
 export function App() {
   return (
     <div>
       <Navbar />
+      <FlightSearchForm/>
       <Footer />
     </div>
   );
