51f8516fe2668f5fe4a7bbbde538dbb01296b601
jangamkeerthi26 <keerthi.jangam@everest.engineeing>
2025-07-15 12:06:03 +0530

    Implement the service which books flight taking flight details and passengers as input

---
+++ src/services/BookFlight.ts
@@ -0,0 +1,25 @@
+import { API_URL } from "../constants/api";
+import type { FlightSearchResult } from "../types/FlightSearchResult";
+
+export const bookFlight = async (
+  flight: FlightSearchResult,
+  passengers: number
+) => {
+  try {
+    const response = await fetch(`${API_URL}book`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ flight, passengers }),
+    });
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(`${data.error}`);
+    }
+    return data.updated;
+  } catch (error) {
+    throw new Error(`${error}`);
+  }
+};
