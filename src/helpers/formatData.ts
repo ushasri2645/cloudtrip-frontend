29282428f038b9421cbb9450e750e6c8785c99b9
NikithaMedikonda <nikitha.medikonda@everest.engineering>
2025-07-21 12:44:54 +0530

    Remove conversion of time

--- src/helpers/formatData.ts
+++ src/helpers/formatData.ts
@@ -1,13 +1,10 @@
 export function formatDateTime(isoString: string): string {
   const dateObj = new Date(isoString);
-  const offsetInMinutes = 5 * 60 + 30;
-  const localTimestamp = dateObj.getTime() + offsetInMinutes * 60 * 1000;
-  const localDate = new Date(localTimestamp);
 
-  const date = localDate.toISOString().split("T")[0];
+  const date = dateObj.toISOString().split("T")[0];
 
-  let hours = localDate.getUTCHours();
-  const minutes = localDate.getUTCMinutes();
+  let hours = dateObj.getUTCHours();
+  const minutes = dateObj.getUTCMinutes();
   const ampm = hours >= 12 ? "PM" : "AM";
   hours = hours % 12;
   if (hours === 0) hours = 12;
