68dc7dea2478883671f3a325af89456efbddf641
nagusha <nagusha.madasu@everest.engineering>
2025-07-21 11:08:59 +0530

    Remove subhead paragraph tests from HeroSection

--- src/components/Hero/HeroSection.test.tsx
+++ src/components/Hero/HeroSection.test.tsx
@@ -2,19 +2,11 @@ import { render, screen } from "@testing-library/react";
 import { Hero } from "./HeroSection";
 
 describe("Hero Component", () => {
-  it("renders the main heading", () => {
+  it("should renders the main heading", () => {
     render(<Hero />);
     const heading = screen.getByRole("heading", {
       name: /Travel Beyond Boundaries/i,
     });
     expect(heading).toBeInTheDocument();
-  });
-
-  it("renders the subheader paragraph", () => {
-    render(<Hero />);
-    const subHeader = screen.getByText(
-      /Search for flights to your dream destinations and book with ease./i
-    );
-    expect(subHeader).toBeInTheDocument();
   });
 });
