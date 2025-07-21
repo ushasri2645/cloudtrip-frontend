f7fcdae644fe7fbab888563546c0eae1fba3e434
nagusha <nagusha.madasu@everest.engineering>
2025-07-21 13:26:46 +0530

    Add tests for TripSelector component

---
+++ src/components/TripSelector/TripSelector.test.tsx
@@ -0,0 +1,39 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import TripSelector from "./TripSelector";
+
+describe("TripSelector Component", () => {
+    it("renders both radio buttons", () => {
+        render(<TripSelector />);
+        expect(screen.getByLabelText(/one way/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/round trip/i)).toBeInTheDocument();
+    });
+
+    it("should selects 'One Way' by default", () => {
+        render(<TripSelector />);
+        const oneWayRadio = screen.getByLabelText(/one way/i) as HTMLInputElement;
+        const roundTripRadio = screen.getByLabelText(/round trip/i) as HTMLInputElement;
+
+        expect(oneWayRadio.checked).toBe(true);
+        expect(roundTripRadio.checked).toBe(false);
+    });
+
+    it("should selects 'Round Trip' when clicked", () => {
+        render(<TripSelector />);
+        const roundTripRadio = screen.getByLabelText(/round trip/i) as HTMLInputElement;
+
+        fireEvent.click(roundTripRadio);
+
+        expect(roundTripRadio.checked).toBe(true);
+        expect((screen.getByLabelText(/one way/i) as HTMLInputElement).checked).toBe(false);
+    });
+
+    it("should allows switching from Round Trip back to One Way", () => {
+        render(<TripSelector />);
+        const oneWayRadio = screen.getByLabelText(/one way/i) as HTMLInputElement;
+        const roundTripRadio = screen.getByLabelText(/round trip/i) as HTMLInputElement;
+        fireEvent.click(roundTripRadio);
+        expect(roundTripRadio.checked).toBe(true);
+        fireEvent.click(oneWayRadio);
+        expect(oneWayRadio.checked).toBe(true);
+    });
+});
